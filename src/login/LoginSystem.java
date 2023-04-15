3ad1e1c9e4e04f3ace64db2599e440cd4d8e9ebb
devinlinux <ssmith969898@gmail.com>
2023-04-15 15:36:07 -0400

    Made passwords file

---
+++ src/login/LoginSystem.java
@@ -0,0 +1,20 @@
+package login;
+
+//  imports
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public final class LoginSystem {
+
+    /* do not let anyone instantiate this class */
+    private LoginSystem() {}
+
+    /* method to create account */
+}
