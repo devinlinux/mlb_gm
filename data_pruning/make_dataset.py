815c482bca9936e0ac515b624f481771737ec92a
devinlinux <ssmith969898@gmail.com>
2023-04-18 13:29:28 -0400

    Modified dataset script to partition data for 2019

--- data_pruning/make_dataset.py
+++ data_pruning/make_dataset.py
@@ -10,8 +10,11 @@ batters_dataset = batters_dataset[['name_common', 'year_ID', 'team_ID']]
 full_dataset = pd.concat([pitchers_dataset, batters_dataset], ignore_index=True)
 
 full_dataset_2023 = full_dataset[full_dataset['year_ID'] == 2023]
+full_dataset_2019 = full_dataset[full_dataset['year_ID'] == 2019]
 
 full_dataset_2023.drop('year_ID', axis=1)
+full_dataset_2019.drop('year_ID', axis=1)
 
 full_dataset.to_csv(os.path.join('data', 'players', 'all_players.csv'), index=False)
 full_dataset_2023.to_csv(os.path.join('data', 'players', '2023_players.csv'), index=False)
+full_dataset_2019.to_csv(os.path.join('data', 'players', '2019_players.csv'), index=False)
