d0fcb12c002c68573990539a897555029d2ec7cd
devinlinux <ssmith969898@gmail.com>
2023-04-18 00:17:55 -0400

    Finished script to prune the data and ran the script to produce the useable data for my purposes

---
+++ data_pruning/make_dataset.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+
+pitchers_dataset = pd.read_csv(os.path.join('data_pruning', 'war_daily_pitch.csv'))
+batters_dataset = pd.read_csv(os.path.join('data_pruning', 'war_daily_bat.csv'))
+
+pitchers_dataset = pitchers_dataset[['name_common', 'year_ID', 'team_ID']]
+batters_dataset = batters_dataset[['name_common', 'year_ID', 'team_ID']]
+
+full_dataset = pd.concat([pitchers_dataset, batters_dataset], ignore_index=True)
+
+full_dataset_2023 = full_dataset[full_dataset['year_ID'] == 2023]
+
+full_dataset_2023.drop('year_ID', axis=1)
+
+full_dataset.to_csv(os.path.join('data', 'players', 'all_players.csv'), index=False)
+full_dataset_2023.to_csv(os.path.join('data', 'players', '2023_players.csv'), index=False)
